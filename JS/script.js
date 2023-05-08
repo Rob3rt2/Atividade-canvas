1397f00b4ef28f7b922bb9b58bcf39454034a405
Rob3rt2 <robert.mota@etec.sp.gov.br>
2023-05-08 12:11:19 -0300

    adicionando novas formas canvas

--- JS/script.js
+++ JS/script.js
@@ -5,18 +5,39 @@ const canvas1 = document.getElementById('canvas1');
 const ctx1 = canvas1.getContext('2d');
 
 const canvas2 = document.getElementById('canvas2');
-const ctx2 =canvas2.getContext('2d');
+const ctx2 = canvas2.getContext('2d');
 
-ctx.moveTo (20, 20);
-ctx.lineTo (200,150);
-ctx.lineTo (20,150);
+const canvas3 = document.getElementById('canvas3');
+const ctx3 = canvas3.getContext('2d');
+
+ctx.moveTo (10, 100);
+ctx.lineTo (65,10);
+ctx.lineTo (120,100);
 ctx.fill();
 
 ctx1.fillStyle = 'red';
 ctx1.fillRect (20,20,170,120);
 ctx1.strokeRect (40, 50, 130, 60);
 
+ctx2.beginPath();
+ctx2.arc(100,100,50,0, Math.PI*2,true);
+ctx2.moveTo(100,200);
+ctx2.stroke();
 
+
+ctx3.beginPath();
+ctx3.moveTo(20, 20);
+ctx3.lineTo (120,20);
+ctx3.lineTo (20, 120);
+ctx3.fill();
+
+
+ctx3.beginPath();
+ctx3.moveTo(140, 140);
+ctx3.lineTo(140, 40);
+ctx3.lineTo(40,140);
+ctx3.closePath();
+ctx3.stroke();
 
 
 
