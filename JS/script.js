abe508aa89cb87dc4e4e3caa48e7bf259e31d777
Rob3rt2 <robert.mota@etec.sp.gov.br>
2023-05-09 09:20:01 -0300

    adicionando curva ao canvas, html

--- JS/script.js
+++ JS/script.js
@@ -10,6 +10,9 @@ const ctx2 = canvas2.getContext('2d');
 const canvas3 = document.getElementById('canvas3');
 const ctx3 = canvas3.getContext('2d');
 
+const canvas4 = document.getElementById('canvas4');
+const ctx4 = canvas4.getContext('2d');
+
 ctx.moveTo (10, 100);
 ctx.lineTo (65,10);
 ctx.lineTo (120,100);
@@ -20,7 +23,7 @@ ctx1.fillRect (60,20,170,120);
 ctx1.strokeRect (80, 50, 130, 60);
 
 ctx2.beginPath();
-ctx2.arc(150,80,70,0, Math.PI*2,true);
+ctx2.arc(150,80,70,0, Math.PI *2,false);
 ctx2.moveTo(100,200);
 ctx2.fillStyle ='red';
 ctx2.fill();
@@ -34,5 +37,10 @@ ctx3.lineTo (170, 70);
 ctx3.stroke();
 
 
-
+ctx4.beginPath();
+ctx4.moveTo(170,69);
+ctx4.quadraticCurveTo(300,120,150,100);
+ctx4.quadraticCurveTo(200,10,20,40);
+ctx4.quadraticCurveTo(100,120,170,69);
+ctx4.stroke();
 
